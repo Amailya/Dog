47766ae3be381571f144fff60ad58d298fca8d12
Amailya <amalyamxitaryan2020@gmail.com>
2025-10-20 22:15:44 +0400

    main

--- main.js
+++ main.js
@@ -1,3 +1,35 @@
-const grid = document.getElementById("grid")
-const LoadBtn = document.getElementById("LoadBtn")
-const apiurl = ''
+const grid = document.getElementById("grid");
+const LoadBtn = document.getElementById("LoadBtn");
+const apiUrl = "https://api.thedogapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=3";
+
+async function fetchdog() {
+  try {
+    const res = await fetch(apiUrl);
+    const data = await res.json();
+    renderGrid(data);
+  } catch (err) {
+    console.log("Error when fetching", err);
+  }
+}
+
+
+function renderGrid(items) {
+  grid.innerHTML = "";
+
+  items.forEach(item => {
+    const card = document.createElement('div');
+    card.className = 'card';
+
+    const img = document.createElement('img');
+    img.className = 'thumb';
+    img.src = item.url;
+    img.alt = item.id;
+
+  
+
+
+    card.appendChild(img);
+    grid.appendChild(card);
+  });
+}
+LoadBtn.addEventListener("click", fetchdog);
