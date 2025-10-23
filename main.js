aa3b95fd5a147edb6da834def1ab3e6268f543f0
Amailya <amalyamxitaryan2020@gmail.com>
2025-10-23 16:17:05 +0400

    main

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 const grid = document.getElementById("grid");
 const LoadBtn = document.getElementById("LoadBtn");
-const apiUrl = "https://api.thedogapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=3";
+const Loading = document.getElementById("Loading");
+const breedSelect = document.getElementById("breedSelect");
+const apiBreeds = 'https://api.thedogapi.com/v1/breeds';
+const apiUrl = 'https://api.thedogapi.com/v1/images/search?size=med&limit=10';
 
-async function fetchdog() {
+async function fetchDogs(breedId = '') {
   try {
+    Loading.style.display = 'block';
     const res = await fetch(apiUrl);
     const data = await res.json();
     renderGrid(data);
@@ -12,10 +16,19 @@ async function fetchdog() {
   }
 }
 
+async function fetchBreeds() {
+  try {
+    const res = await fetch(apiBreeds);
+    const data = await res.json();
+    renderBreeds(data);
+  } catch (err) {
+    console.log("Error when fetching", err);
+  }
+}
 
 function renderGrid(items) {
   grid.innerHTML = "";
-
+  Loading.style.display = 'none';
   items.forEach(item => {
     const card = document.createElement('div');
     card.className = 'card';
@@ -25,11 +38,26 @@ function renderGrid(items) {
     img.src = item.url;
     img.alt = item.id;
 
-  
-
-
     card.appendChild(img);
     grid.appendChild(card);
   });
 }
-LoadBtn.addEventListener("click", fetchdog);
+
+function renderBreeds(items){
+  breedSelect.innerHTML = '<option value="">dog breed</option>';
+  items.forEach((breed) => {
+    const option = document.createElement('option');
+    option.value = breed.id;
+    option.textContent = breed.name;
+    breedSelect.appendChild(option);
+  });
+}
+
+LoadBtn.addEventListener('click', () => fetchDogs());
+
+breedSelect.addEventListener('change', () => {
+  fetchDogs(breedSelect.value);
+});
+
+fetchBreeds();
+fetchDogs();
